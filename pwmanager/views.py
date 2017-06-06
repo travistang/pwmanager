6763558ddc5caff033ed3d6af9e6a13e395a2c26
Travis tang <yiutingtang@gmail.com>
2017-06-06 22:33:18 +0900

    Delete function is working but something needs to be done to solve the duplicating name issue

--- pwmanager/views.py
+++ pwmanager/views.py
@@ -18,7 +18,8 @@ def send_password(obj):
 	Group('users').send({'text':json.dumps(json_friendly(obj))})
 # TODO: correct me?
 def send_delete_notice(obj):
-	Group('users').send({'deleted':json.dumps(json_friendly(obj))})
+	print('sending notice:',json.dumps(obj))
+	Group('users').send({'text':json.dumps(obj)})
 # Create your views here.
 def index(request):
 	passwords = Password.objects.order_by('-name')
@@ -29,19 +30,21 @@ def index(request):
 
 # TODO: correct me
 def remove(request):
-	req_dict = parse_qs(request.body)
+	request = request.body.decode('utf-8')
+	req_dict = json.loads(request)
+	print(request)
 	response = {}
-	if b'name' not in req_dict:
+	if 'name' not in req_dict:
 		response['status'] = 'fail'
 	else:
-		name = req_dict[b'name'][0].decode('utf-8')
+		name = req_dict['name']
 		pw_entry = Password.objects.get(name = name)
 		if not pw_entry:
 			response['status'] = 'fail'
 		else:
 			pw_entry.delete()
 			response['status'] = 'ok'
-	send_delete_notice(name)
+		send_delete_notice({'action': 'deleted','name':name})
 	return JsonResponse(response)
 def create(request):
 	print(request)
